8f7866aec61c09def509a96a4be8732eda627086
Roman <krasevych@animatron.com>
2016-05-19 14:14:42 +0300

    change logic for Function name

--- dist/angular-module-injector.js
+++ dist/angular-module-injector.js
@@ -1,11 +1,15 @@
-/** angular-module-injector v0.0.1
+/** angular-module-injector v0.0.3
     * (c) 2016 @krasevych
     * License: MIT
     * https://github.com/krasevych/angular-module-injector
     */ 
 (function () {
     var ng = angular.module;
-    var mapModuleName = function (moduleName) { return moduleName instanceof Function ? moduleName.name : moduleName; };
+    var mapModuleName = function (moduleName) {
+        return moduleName instanceof Function
+            ? moduleName.toString().match(/^function\s*([^\s(]+)/)[1]
+            : moduleName;
+    };
     function module(name, requires, configFn) {
         name = mapModuleName(name);
         if (requires) {
